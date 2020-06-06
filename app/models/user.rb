49268a54925c366a175e50cae3b11d1bc3b5151c
Jessica McElroy <mcelroyjessica@gmail.com>
2020-06-06 10:57:36 -0700

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-  has_many :articles
+  has_many :articles, dependent: :destroy
+  
+  validates :name, presence: true, uniqueness: { case_sensitive: false },
+    length: { minimum: 3, maximum: 25 }
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { maximum: 105 },
+    uniqueness: { case_sensitive: false },
+    format: { with:  VALID_EMAIL_REGEX }
 end
