c182fe647534ffd11a7eebc1c03ed9867f3cd998
Jessica McElroy <mcelroyjessica@gmail.com>
2020-06-07 21:11:01 -0700

    allow admin users to take all actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -60,7 +60,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user || !current_user.admin?
       flash[:alert] = "You are unauthorized to access that"
       redirect_to @article
     end
